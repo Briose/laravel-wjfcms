20f138adb66b2e5204165ce79b77ae50d4f8e95c
Vijay <1937832819@qq.com>
2020-06-08 09:38:55 +0800

    '修改默认文章'

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -19,13 +19,9 @@ class ArticlesTableSeeder extends Seeder
                 'title' => '记录生活，记录成长',
                 'author' => 'Vijay',
                 'slug' => 'php',
-                'content' => '录生活，记录成长。&lt;/h1&gt;
-                &lt;p&gt;时间过得太快，不去留意，不去记录，不去总结。只会使人活得麻木和安逸，等哪一天你想回忆时，你会发现，回忆是那么模糊...&lt;/p&gt;&lt;p&gt;每一份经历，当你把每一次成长，每一段时光都值得被记录，它们将会是你未来的财富。',
-                'markdown' => '&lt;p&gt;&lt;/p&gt;&lt;div class=&quot;about_text text_about&quot;&gt;
-                &lt;h1&gt;记录生活，记录成长。&lt;/h1&gt;
-                &lt;p&gt;时间过得太快，不去留意，不去记录，不去总结。只会使人活得麻木和安逸，等哪一天你想回忆时，你会发现，回忆是那么模糊...&lt;/p&gt;&lt;p&gt;每一份经历，当你把每一次成长，每一段时光都值得被记录，它们将会是你未来的财富。&lt;/p&gt;
-            &lt;/div&gt;&lt;p&gt;&lt;/p&gt;',
-                'description' => '时间过得太快，不去留意，不去记录，不去总结。只会使人活得麻木和安逸，等哪一天你想回忆时，你会发现，回忆是那么模糊...每一份经历，当你把每一次成长，每一段时光都值得被记录，它们将会是你未来的财富。',
+                'content' => '&lt;p&gt;laravel-wjfcms是基于laravel及layui开发的后台管理系统。&lt;/p&gt;',
+                'markdown'=>'laravel-wjfcms是基于laravel及layui开发的后台管理系统。',
+                'description'=>'laravel-wjfcms是基于laravel及layui开发的后台管理系统。',
                 'keywords' => 'laravel',
                 'cover' => '',
                 'is_top' => 1,
