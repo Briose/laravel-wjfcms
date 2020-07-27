93c1244921dd0db87734621ee75aa8a18a6dbfe7
Vijay <1937832819@qq.com>
2020-07-27 16:17:53 +0800

    's'

--- resources/views/layouts/home/about.blade.php
+++ resources/views/layouts/home/about.blade.php
@@ -3,9 +3,9 @@
         <img src="/images/config/avatar.jpg" alt="Vijay" title="Vijay">
     </div>
     <p class="abname">Vijay</p>
-    <p class="abposition">拍黄片(PHP)开发工程师</p>
+    <p class="abposition">后端开发工程师</p>
     <p style="text-align: center;">
         <a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&amp;email=1937832819@qq.com" rel="nofollow" target="_blank">1937832819@qq.com</a>
     </p>
-    <div class="abtext">每一次经历，每一段时光都值得被记录，它们将会是你未来的财富。</div>
+    <div class="abtext">如果没有天赋异禀，那请选择励精图治。</div>
 </div>
